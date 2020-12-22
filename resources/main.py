fdca643c0814be1c07555d3cc848a5d4b521a50a
Gabriela Barrera <g.barrera.angel@gmail.com>
2020-12-22 16:15:34 -0300

    corregida funcion para transformar dat a csv

--- resources/main.py
+++ resources/main.py
@@ -45,27 +45,18 @@ def dat_files_clean(files_names):
 
     """
 
-    for f in files_names:
-        #print("\n")
-        with open(f, 'r', encoding='iso-8859-1') as input_file:
-            # print("Nombre del archivo: " + f + "\n")
-            lines = input_file.readlines()
-            filtered_file = []
-            #print(lines)
-            num_lineas = 0
-            for linea in lines:
-                print(linea)
-                num_lineas += 1
-                #linea = linea.split(';')
-
-                if linea.startswith('Tipo Transacc'):
-                    # print(linea)
-                    # print(num_lineas)
-                    break
-            # print(num_lineas)
-            filtered_file = lines[num_lineas-1:]
-            # print(filtered_file)
-        #save_files(f, filtered_file)
+    #for f in files_names:
+    with open(files_names, 'r', encoding='iso-8859-1') as input_file:
+        lines = input_file.readlines()
+        filtered_file = []
+        num_lineas = 0
+        for linea in lines:
+            #print(linea)
+            num_lineas += 1
+            if linea.startswith('Tipo Transacc'):
+                break
+        filtered_file = lines[num_lineas-1:]
+        #print(filtered_file)
     return filtered_file
 
 
@@ -91,17 +82,12 @@ def bsale_clean_file(file):
     with open(file, 'r', encoding='utf-8') as input_file:
         lines = input_file.readlines()
         newlines = []
-        #print(lines)
         i = 0
         for linea in lines:
-            # print(linea)
             i += 1
             linea = linea.split(';')
             if linea[1] == 'Tipo Documento' and linea[2] == 'Nº Documento':
-                #print(linea)
-                #print(i)
                 break
-        #print(i)
         newlines = lines[i-1:]
     return newlines
 
@@ -121,31 +107,15 @@ def save_files(output_file_name, content):
     """
 
     # Obtengo solo el nombre de los archivos, sin extensión
-    file_names = [os.path.splitext(f)[0] for f in output_file_name]
-    print(file_names)
-    # print(content)
+    
+    file_names = []
+    for elemento in output_file_name:
+        head, tail = os.path.split(elemento)
+        tail = tail.replace('.dat', '')
+        file_names.append(tail)
 
     for f in file_names:
-        new_files_csv = '{}.{}'.format(f, 'csv')
+        new_files_csv = '{}/{}_{}.{}'.format(head, 'eq_beta', f, 'csv')
         with open(new_files_csv, 'w') as output_file:
             for linea in content:
                 output_file.write(linea)
-
-
-def get_file_encoding(src_file_path):
-    """Obtiene el tipo de codificación de un archivo
-
-    Parameters
-    ----------
-    src_file_path : str
-        Ruta del archivo
-
-    Returns
-    -------
-
-    src_file : str
-        Tipo de codificación del archivo
-    """
-
-    with open(src_file_path) as src_file:
-        return src_file.encoding
